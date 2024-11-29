ac6ed6ee71e21568711a5ebb973fff10557cb7a0
qwang0504 <qing.wang@bi.mpg.de>
2024-11-29 17:36:48 +0100

    added whole new metadata class for getting all kinds of metadata from camera_control and stimmanager - all not tested

--- metadata.py
+++ metadata.py
@@ -1,27 +1,199 @@
-import numpy as np
+# import numpy as np
+# import pandas as pd
 from stimulation import StimManager
-from LED import LEDDriver
+# from LED import LEDDriver
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QGroupBox, QLineEdit, QCalendarWidget, QFileDialog
+from PyQt5.QtCore import QDate
+from camera_widgets_new import CameraControl
+import json
+from pathlib import Path
 
-class Metadata:
+class Metadata(QWidget):
     def __init__(
             self, 
             stim_manager: StimManager, 
+            cam_controls: CameraControl,
+            # led_driver: LEDDriver,
             *args, 
             **kwargs
             ):
+        
+        super().__init__(*args, **kwargs)
+
         self.stim_manager = stim_manager 
+        self.cam_controls = cam_controls
+        # self.led_driver = led_driver
+
+        self.declare_components()
+        self.layout_components()
+
+    def initialise_widget(self, signal: int):
+        if signal: 
+            self.show()
+            self.generate_id()
+            self.get_video_settings()
+            self.get_interval()
+            self.get_directory()
+            self.get_mask_order()
+            self.get_interval()
+            self.get_pulse_timing()
+
+    def declare_components(self):
+
+        self.fish_number_input = QLineEdit(self)
+        self.fish_number_input.setText('Fish number (fn)')
+        self.fish_number_input.returnPressed.connect(self.set_fish_number)
+        self.fish_number_input_instructions = QLabel(self)
+        self.fish_number_input_instructions.setText('001, 002 etc.')
+        
+        self.calendar = QCalendarWidget(self)
+        self.calendar.setGridVisible(True)
+        self.calendar.selectionChanged.connect(self.calculate_age)
+
+        self.calendar_label = QLabel(self)
+        self.calendar_label.setText('Date of birth: ')
+
+        self.dpf_label = QLabel(self)
+        self.dpf_label.setText('Days post-fertilisation: ')
+
+        self.fishline_input = QLineEdit(self)
+        self.fishline_input.setText('Fish line')
+        self.fishline_input.returnPressed.connect(self.set_fishline)
+
+        self.condition_input = QLineEdit(self)
+        self.condition_input.setText('Condition')
+        self.condition_input.returnPressed.connect(self.set_condition)
+
+        # self.directory_button = QPushButton(self)
+        # self.directory_button.setText('Select directory')
+        # self.directory_button.clicked.connect(self.set_directory)
+        # self.directory_label = QLabel(self)
+        # self.directory_label.setText('Selected directory: ')
+
+        self.export_metadata_button = QPushButton(self)
+        self.export_metadata_button.setText('Export metadata')
+        self.export_metadata_button.clicked.connect(self.export_metadata)
+
+    def layout_components(self):
+        layout = QVBoxLayout(self)
+        
+        layout.addWidget(self.fish_number_input_instructions)
+        layout.addWidget(self.fish_number_input)
+
+        layout.addWidget(self.calendar_label)
+        layout.addWidget(self.calendar)
+        layout.addWidget(self.dpf_label)
+
+        layout.addWidget(self.fishline_input)
+        
+        layout.addWidget(self.condition_input)
+        layout.addWidget(self.export_metadata_button)
+
+
+    # gets today's date and time and formats it into YYYYmmDDHHMM
+    # creates unique id with fish number 
+    def generate_id(self):
+        date = self.cam_controls.sender.date
+        self.id = date + self.fish_number
     
+    def set_fish_number(self):
+        self.fish_number = self.fish_number_input.text()
     
-    # def generate_id(self):
-        #date, fish number
+    def set_fishline(self):
+        self.fishline = self.fishline_input.text()
+
+    def set_condition(self):
+        self.condition = self.condition_input.text()
+    
+    def calculate_age(self):
+        # Get the selected date
+        dob = self.calendar.selectedDate()
+        # Format the date as a string
+        dob_str = dob.toString("yyyy-MM-dd")
+        self.dob = dob.toString("yyyyMMdd")
+        # Update the label with the selected date
+        self.calendar_label.setText(f"Date of birth: {dob_str}")
+        today_qdate = QDate(self.today.year, self.today.month, self.today.day)
+        self.age = today_qdate.daysTo(dob)
+        self.dpf_label.setText(f'Days post-fertilisation: {self.age}')
+        print(self.age)
+
+    def get_video_settings(self):
+        self.fps = self.cam_controls.camera.get_framerate()
+        self.exposure = self.cam_controls.camera.get_exposure()
+        self.gain = self.cam_controls.camera.get_gain()
+        self.width = self.cam_controls.camera.get_width()
+        self.height = self.cam_controls.camera.get_height()
+        self.fourcc = self.cam_controls.sender.fourcc
+        self.video_filename = self.cam_controls.sender.filename
+        self.video_start_time = self.cam_controls.sender.video_start_time 
+
+    def get_interval(self):
+        self.interval = self.stim_manager.interval_spinbox.value()
+
+    def get_mask_order(self):
+        if self.stim_manager.shuffled_mask_keys:
+            self.mask_order = self.stim_manager.shuffled_mask_names
+
+    def get_pulse_timing(self, signal: int):
+        if signal: 
+            self.pulse_start = self.stim_manager.start_stim.pulse_start
+            self.pulse_end = self.stim_manager.start_stim.pulse_end
+            self.pulse_duration = self.stim_manager.start_stim.pulse_duration
+            print(self.pulse_start)
+            print(self.pulse_end)
+            print(self.pulse_duration)
+        
+    def get_directory(self):
+        self.directory = self.cam_controls.sender.file_dir
+        # self.directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        # if self.directory: 
+        #     self.directory_label.setText(f'Selected directory: {self.directory}')
+
+    def export_metadata(self):
+        metadata_dict = {
+            'fish_id': self.id, 
+            'line': self.fishline,
+            'condition': self.condition, 
+            'dob': self.dob, 
+            'age': self.age, 
+            'fps': self.fps, 
+            'exposure': self.exposure, 
+            'gain': self.gain, 
+            'frame_width': self.width, 
+            'frame_height': self.height, 
+            'fourcc': self.fourcc, 
+            'video_filename': self.video_filename,
+            'video_start': self.video_start_time, 
+            'interval': self.interval, 
+            'mask_order': self.mask_order, 
+            'pulse_start': list(self.pulse_start), 
+            'pulse_end': list(self.pulse_end), 
+            'pulse_duration': list(self.pulse_duration)
+        }
+
+        metadata_path = Path(self.directory, self.id+'.json')
+
+        with open(metadata_path, 'w') as file:
+            json.dump(metadata_dict, file)
 
     #genotype, age, condition
-    #mask exposed, pulse start, pulse end, duration 
-    #fps, exposure, gain, encoding 
+    #mask exposed, pulse start, pulse end, duration, interval 
+    #fps, exposure, gain, encoding, frame size, fourcc, filename, video start time
+    #LED SETTINGS!!!!!!! duty cycle, intensity, frequency 
 
     #write json with id
 
-    
+import os 
+import json
 
+test_dict = {
+    1: 1, 
+    2: '2', 
+    3: [1,2,3,4]
+}
 
+dir = Path(os.getcwd(), 'test_dict.json')
+with open(dir, 'w') as file: 
+    json.dump(test_dict, file)
 
