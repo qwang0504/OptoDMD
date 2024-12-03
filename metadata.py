8e60da97023616059e98c377a596c1337232fc3f
WANGQING <qing.wang@bi.mpg.de>
2024-12-03 12:11:00 +0100

    added cameraMetadata class for exporting camera-based metadata and fish-related metadata

--- metadata.py
+++ metadata.py
@@ -9,6 +9,7 @@ from camera_widgets_new import CameraControl
 import json
 from pathlib import Path
 from datetime import datetime
+import zmq
 
 class Metadata(QWidget):
     def __init__(
@@ -203,6 +204,137 @@ class Metadata(QWidget):
 
     #write json with id
 
+
+class CameraMetadata(QWidget):
+    def __init__(
+            self, 
+            cam_controls: CameraControl,
+            *args, 
+            **kwargs
+            ):
+        
+        super().__init__(*args, **kwargs)
+
+        self.cam_controls = cam_controls
+
+        self.declare_components()
+        self.layout_components()
+
+
+    def initialise_widget(self, signal: int):
+        if signal: 
+            self.show()
+            self.get_id()
+            self.get_video_settings()
+            self.get_directory()
+
+    def declare_components(self):
+        
+        self.calendar_label = QLabel(self)
+        self.calendar_label.setText('Date of birth: ')
+        
+        self.calendar = QCalendarWidget(self)
+        self.calendar.setGridVisible(True)
+        self.calendar.selectionChanged.connect(self.calculate_age)
+
+        self.dpf_label = QLabel(self)
+        self.dpf_label.setText('Days post-fertilisation: ')
+
+        self.fishline_label = QLabel(self)
+        self.fishline_label.setText('Fish line: ')
+
+        self.fishline_input = QLineEdit(self)
+        self.fishline_input.returnPressed.connect(self.set_fishline)
+
+        self.condition_label = QLabel(self)
+        self.condition_label.setText('Fish condition (e.g. control, blind)')
+
+        self.condition_input = QLineEdit(self)
+        self.condition_input.returnPressed.connect(self.set_condition)
+
+        self.export_metadata_button = QPushButton(self)
+        self.export_metadata_button.setText('Export metadata')
+        self.export_metadata_button.clicked.connect(self.export_metadata)
+
+    def layout_components(self):
+        layout = QVBoxLayout(self)
+
+        layout.addWidget(self.calendar_label)
+        layout.addWidget(self.calendar)
+        layout.addWidget(self.dpf_label)
+
+        layout.addWidget(self.fishline_label)
+        layout.addWidget(self.fishline_input)
+
+        layout.addWidget(self.condition_label)
+        layout.addWidget(self.condition_input)
+        
+        layout.addWidget(self.export_metadata_button)
+
+    # gets today's date and time and formats it into YYYYmmDDHHMM
+    # creates unique id with fish number 
+    def get_id(self):
+        self.id = self.cam_controls.fish_id
+    
+    def set_fishline(self):
+        self.fishline = self.fishline_input.text()
+
+    def set_condition(self):
+        self.condition = self.condition_input.text()
+    
+    def calculate_age(self):
+        # Get the selected date
+        dob = self.calendar.selectedDate()
+        # Format the date as a string
+        dob_str = dob.toString("yyyy-MM-dd")
+        self.dob = dob.toString("yyyyMMdd")
+        # Update the label with the selected date
+        self.calendar_label.setText(f"Date of birth: {dob_str}")
+        self.today = datetime.today()
+        today_qdate = QDate(self.today.year, self.today.month, self.today.day)
+        # self.age = today_qdate.daysTo(dob)
+        self.age = dob.daysTo(today_qdate) - 1
+        self.dpf_label.setText(f'Days post-fertilisation: {self.age}')
+        print(self.age)
+
+    def get_video_settings(self):
+        self.fps = self.cam_controls.camera.get_framerate()
+        self.exposure = self.cam_controls.camera.get_exposure()
+        self.gain = self.cam_controls.camera.get_gain()
+        self.width = self.cam_controls.camera.get_width()
+        self.height = self.cam_controls.camera.get_height()
+        self.fourcc = self.cam_controls.sender.fourcc
+        self.filename = self.cam_controls.sender.filename
+        self.video_start_time = self.cam_controls.sender.video_start_time 
+
+    def get_directory(self):
+        self.directory = self.cam_controls.fish_dir
+        # self.directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        # if self.directory: 
+        #     self.directory_label.setText(f'Selected directory: {self.directory}')
+
+    def export_metadata(self):
+        metadata_dict = {
+            'fish_id': self.id, 
+            'line': self.fishline,
+            'condition': self.condition, 
+            'dob': self.dob, 
+            'age': self.age, 
+            'fps': self.fps, 
+            'exposure': self.exposure, 
+            'gain': self.gain, 
+            'frame_width': self.width, 
+            'frame_height': self.height, 
+            'fourcc': self.fourcc, 
+            'video_filename': self.filename,
+            'video_start': self.video_start_time, 
+        }
+
+        metadata_path = Path(self.directory, self.filename+'_camera.json')
+
+        with open(metadata_path, 'w') as file:
+            json.dump(metadata_dict, file)
+
 import os 
 import json
 
@@ -215,4 +347,3 @@ test_dict = {
 dir = Path(os.getcwd(), 'test_dict.json')
 with open(dir, 'w') as file: 
     json.dump(test_dict, file)
-
