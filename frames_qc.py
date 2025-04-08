2bb0cab7596cd881dc080cbcbbc11cd0ecd81a56
qwang0504 <qing.wang@bi.mpg.de>
2025-04-08 11:22:25 +0200

    update conditions

--- frames_qc.py
+++ frames_qc.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dtype = [('index', int), ('timestamp', np.float64)]
+dtype = [('index', int), ('timestamp', np.float32)]
 
-cam = np.loadtxt('cam_frames_AQ_200.txt', delimiter=',', dtype=dtype)
-sink = np.loadtxt('sink_frames_AQ_200.txt', delimiter=',', dtype=dtype)
+cam = np.loadtxt('cam_frames_threads_AQ_highres_record_200_sentinel(4).txt', delimiter=',', dtype=dtype)
+sink = np.loadtxt('record_frames_threads_AQ_highres_200_sentinel(4).txt', delimiter=',', dtype=dtype)
 
 plt.plot(np.diff(cam['timestamp']))
 plt.plot(np.diff(sink['timestamp']))
