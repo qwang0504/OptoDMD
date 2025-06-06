3dc14b3e11d063ddd2897b46ec39cf34fa522c76
WANGQING <qing.wang@bi.mpg.de>
2025-06-06 18:23:20 +0200

    scanimage user functions and config

--- stop_trigger.m
+++ stop_trigger.m
@@ -1,14 +1,14 @@
 function trigger_recording(src, evt, args)
-    disp('Start triggered')
+    disp('Stop triggered')
     disp(['Got event: ' evt.EventName])
 %     #check if socket is setup
 %         if not: wanring/start_zmq
 %         
     global socket
     % global socketstim
-    if src.hSI.acqState ~= "focus"
+    if src.hSI.acqState = "Abort"
         disp('Sending')
-        socket.send("start")
+        socket.send("stop")
         % socketstim.send("start")
     end
 end
